61a2c49c84f2f308eaab768a186ef96616e6a673
hannahdouglas <hmdougla@andrew.cmu.edu>
2018-08-03 12:57:33 -0400

    added function model_select(covariates, response, cutoff

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -7,3 +7,18 @@ generate_data = function(n,p){
   return(list(covariates = mat, responses = vec ))
 }
 generate_data(3,2)
+
+
+model_select = function(covariates, responses, cutoff){
+  linreg = lm(responses~covariates)
+  sum.linreg.p = summary(linreg)$coefficients[,"Pr(>|t|)"]
+  new.covariates = c()
+  for (i in 2:(ncol(covariates)+1)){
+    if (sum.linreg.p[i]<= cutoff){
+      new.covariates = c(new.covariates, (i-1))
+    }
+  }
+  new.linreg = lm(responses~ covariates[, new.covariates])
+  sum.new.linreg.p = summary(new.linreg)$coefficients[,"Pr(>|t|)"]
+  return(sum.new.linreg.p)
+}
